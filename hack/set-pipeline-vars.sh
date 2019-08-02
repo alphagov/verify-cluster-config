91159844c983a427fe3ee753343a16a23184391a
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2019-08-02 11:45:43 +0100

    Add hack script to manually set pipelines
    - The Concourse pipeline doesn't have these variables set by Concourse
      from the start, so we need to specify them. Like in `alphagov/gsp`, add
      a `hack/` script to set pipeline vars.
    - The multi-tenant Concourse is v4, and the GSP Concourses are v5.
    - Fly does not like mismatching major versions.
    - I have got around needing to `fly` in both Concourses by downloading
      v4's `fly` and using it as `fly4`.
    - If `FLY_BIN` is unset, use `fly`.
    
    Co-authored-by: Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
    Co-authored-by: Issy Long <isabell.long@digital.cabinet-office.gov.uk>
    Co-authored-by: Daniel Blair <daniel.blair@digital.cabinet-office.gov.uk>

---
+++ hack/set-pipeline-vars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+: "${CLUSTER_NAME:?}"
+PIPELINE_NAME="${PIPELINE_NAME:-vpc-endpoint}"
+FLY_BIN="${FLY_BIN:-fly}"
+
+$FLY_BIN -t cd-gsp sync
+
+$FLY_BIN -t cd-gsp set-pipeline -p "${PIPELINE_NAME}" \
+	--config "ci/psn.yaml" \
+  --var "account-name=${CLUSTER_NAME}" \
+  --var "cluster-name=${CLUSTER_NAME}" \
+  --var "concourse-pipeline-name=${PIPELINE_NAME}" \
+  --var "concourse-team=gsp" \
+  --yaml-var "github-approvers=[noone]" \
+  --var "github-resource-image=govsvc/concourse-github-resource" \
+  --var "github-resource-tag=gsp-v1.0.44" \
+  --yaml-var "trusted-developer-keys=[]" \
+	--check-creds "$@"
+
+$FLY_BIN -t cd-gsp expose-pipeline -p "${PIPELINE_NAME}"
