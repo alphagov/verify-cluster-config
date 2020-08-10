c3bcd67c71f062efc7f3fc040efe10d2711d4a2f
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2020-08-10 17:37:28 +0100

    And this

--- customdomain-terraform/modules/customdomain/main.tf
+++ customdomain-terraform/modules/customdomain/main.tf
@@ -52,7 +52,7 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "cert-validation" {
   provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 resource "aws_cloudfront_distribution" "cloudfront_dist" {
